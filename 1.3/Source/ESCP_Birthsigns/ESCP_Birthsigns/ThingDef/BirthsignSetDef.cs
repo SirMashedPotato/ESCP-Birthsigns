f5422e43c2ec8c7bdca280702d95880fe368cf17
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-07-27 09:34:08 +1200

    Adding in ConfigErrors

--- 1.3/Source/ESCP_Birthsigns/ESCP_Birthsigns/ThingDef/BirthsignSetDef.cs
+++ 1.3/Source/ESCP_Birthsigns/ESCP_Birthsigns/ThingDef/BirthsignSetDef.cs
@@ -15,7 +15,49 @@ namespace ESCP_Birthsigns
 
         public override IEnumerable<string> ConfigErrors()
         {
-            return base.ConfigErrors();
+            foreach (string text in base.ConfigErrors())
+            {
+                yield return text;
+            }
+            if (label == null || label == "")
+            {
+                yield return "label is null or blank";
+            }
+            if (description == null || description == "")
+            {
+                yield return "desciption is null or blank";
+            }
+            ///Checking to make sure birthsignHediffs is set up correctly
+            bool flag = false;
+            if (birthsignHediffs.NullOrEmpty())
+            {
+                yield return "birthsignHediffs is null or empty";
+                flag = true;
+            } 
+            else
+            {
+                if (birthsignHediffs.Count != 4)
+                {
+                    yield return "birthsignHediffs does not contain exactly 4 season groups";
+                    flag = true;
+                } 
+                else
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (birthsignHediffs[i].Count != 3)
+                        {
+                            yield return "birthsignHediffs does not contain exactly 3 hediffs in season group: " + (i+1) + ", starting with: " + (birthsignHediffs[i].NullOrEmpty() ? "Null" : birthsignHediffs[i][0].defName);
+                            flag = true;
+                        }
+                    }
+                }
+            }
+            if (flag)
+            {
+                yield return "errors found, this will cause out of bounds exceptions";
+            }
+            yield break;
         }
     }
 }
